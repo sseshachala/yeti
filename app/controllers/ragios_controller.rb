0f61ab3daec981c137abb6a885358ebbbfa7fad1
Obi Akubue <obioraakubue@yahoo.com>
2011-03-11 01:59:29 -0500

    Added ability to test monitors on real-time from the web UI and display results

--- app/controllers/ragios_controller.rb
+++ app/controllers/ragios_controller.rb
@@ -7,10 +7,37 @@ class RagiosController < ApplicationController
  def init
     
    Whisper::Monitors.start
+   redirect_to :action => 'monitors'
  end
 
  def monitors
       @monitors = Whisper::Monitors.get_monitors
  end
+
+ def test_monitor
+     @id = params[:id].to_i
+     monitors = Whisper::Monitors.get_monitors
+     @monitor = monitors[@id]
+     @monitor.test_command
+
+     begin 
+          @monitor.time_of_last_test = Time.now 
+ 	  if @monitor.test_command
+             @monitor.num_tests_passed = @monitor.num_tests_passed + 1
+             @status = "PASSED"
+  	  else
+           @monitor.num_tests_failed = @monitor.num_tests_failed + 1
+           @monitor.failed
+           @status = "FAILED"
+  	  end
+   	  
+	rescue Exception
+           @status = "FAILED"
+           @monitor.error_handler
+           raise
+        end
+       @monitor.total_num_tests = @monitor.total_num_tests + 1   
+
+ end 
 
 end
