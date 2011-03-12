4988b1cbc9ba491e58e2ce420b48e7a70d48d38c
Obi Akubue <obioraakubue@yahoo.com>
2011-03-12 00:37:00 -0500

    Ragios notifier config is stored in config/environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,16 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Yeti::Application.initialize!
 
+#For Twitter notifications
+#@@consumer_key = 'Consumer Key'
+#@@consumer_secret = 'Consumer secret'
+#@@access_token = 'access token'
+#@@access_token = 'access secret' 
+
+#For Gmail notifications 
+# @@username = 'gmail_username'  #replace with gmail username
+# @@password =  'gmail_password' #replace with gmail password
+
 #Add your monitors here
 @@monitoring =  { :monitor => 'url',
                    :every => '15m',
