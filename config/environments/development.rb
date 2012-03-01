52121907edb8656420ef43506f4af3660abe81ee
Obi Akubue <obioraakubue@yahoo.com>
2012-02-29 23:44:12 -0500

    setting up mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,5 +22,16 @@ Yeti::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => 'sl.myslhome.tv',
+  :user_name            => '<username>',
+  :password             => '<password>',
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  }
+
 end
 
