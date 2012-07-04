22928054005e7e5f7b20556514a5309e115ff51c
obi-a <obioraakubue@yahoo.com>
2012-07-04 04:09:47 -0400

    added specs to validate, create, destroy user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
+
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+ # pending "add some examples to (or delete) #{__FILE__}"
+
+
+ it { User.should include(ActiveModel::Validations) }
+
+ it "should create a user" do
+   
+  hash = {"username" =>"test_user", "email" => "test@yeti.com","password" => "trusted", "password_confirmation" => "trusted"}
+   @user = User.new(hash)
+   hash = @user.create_user
+   hash.include?("ok").should == true
+   hash.include?("rev").should == true
+   hash["id"].should == 'org.couchdb.user:test_user'
+  end
+
+  it "should delete a user" do
+   @user = User.find_object("test_user")
+   hash = @user.destroy
+   hash.include?("ok").should == true
+   hash.include?("rev").should == true
+   hash["id"].should == 'org.couchdb.user:test_user'
+  end
+
 end
