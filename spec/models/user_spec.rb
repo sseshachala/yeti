5e14d287f54f6c27a00ce48ae42b6cd859094d4d
obi-a <obioraakubue@yahoo.com>
2012-07-04 20:11:13 -0400

    find user, update password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,6 +26,43 @@ describe User do
      
   end
 
+  it "should find user" do
+  
+   hash = User.find("test_user")
+   hash["name"].should == 'test_user'
+   hash["email"].should == 'test@yeti.com'
+   hash["_id"].should == 'org.couchdb.user:test_user'
+
+   @user = User.find_object("test_user")
+   @user.username.should == 'test_user'
+   @user.email.should == 'test@yeti.com'
+   @user.attributes["name"].should == 'test_user'
+   @user.attributes["email"].should == 'test@yeti.com'
+  end
+
+  it "should update user's email" do
+       
+    current_user = User.find_object("test_user")
+    user_hash = {"email"=>"new.email@yeti.com", "password"=>"", "password_confirmation"=>""}
+    @user = current_user
+    hash = @user.update_email(user_hash,current_user)
+    hash.include?("ok").should == true
+    hash.include?("rev").should == true
+    hash["id"].should == 'org.couchdb.user:test_user'
+
+
+    #do if email was not changed
+    current_user = User.find_object("test_user")
+    user_hash = {"email"=>"new.email@yeti.com", "password"=>"", "password_confirmation"=>""}
+    @user = current_user
+    @user.update_email(user_hash,current_user).should == nil 
+
+   end
+
+  it "should update password" do
+
+  end
+
   it "should delete a user" do
    @user = User.find_object("test_user")
    hash = @user.destroy
