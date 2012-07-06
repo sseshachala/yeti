358c58295a2b3114e91466a4271cb9f9b79bc19d
obi-a <obioraakubue@yahoo.com>
2012-07-06 04:03:32 -0400

    completing test coverage for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -69,6 +69,53 @@ describe User do
     hash["id"].should == 'org.couchdb.user:test_user'
   end
 
+ it "should get all users" do
+   hash = User.all
+   hash[0].include?("username").should == true
+   hash[0].include?("email").should == true
+ end
+ 
+ it "should check if user is an admin" do
+   #assumes that the admin user yeti has been created
+   #otherwise disable this test, it would fail unless an admin user yeti exists
+   #for security reasons admins can only be created directly on couchdb.
+   @user = User.find_object("yeti")
+   @user.admin?.should == true  
+
+   @user = User.find_object("test_user")
+   @user.admin?.should == false     
+ end
+
+
+ it "should authenticate with salt" do
+   hash = User.find("test_user")
+   cookie_salt = hash["salt"]
+   @user = User.authenticate_with_salt("test_user", cookie_salt)
+   @user.username.should == 'test_user'
+   @user.attributes["name"].should == 'test_user'
+   @user.attributes["_id"].should == 'org.couchdb.user:test_user'
+
+   User.authenticate_with_salt("test_user", "wrong_salt").should == nil
+   User.authenticate_with_salt(nil, nil).should == nil
+ end
+
+  it "should find user with forgotten emails" do
+    User.found_user_with_forgotten_email?("new.email@yeti.com").should == true
+    User.found_user_with_forgotten_email?("unknown.email@yeti.com").should == false
+  end
+
+  it "should create a password reset code" do
+    password_reset_code = User.password_reset_code('new.email@yeti.com')
+    hash = User.find("test_user")
+    password_reset_code.should == hash["name"] + "/" + hash["password_reset_code"]
+    User.password_reset_code('wrong@email.com').should == nil
+  end
+
+   it "should check if password reset code is valid" do
+    #true.should == false
+    #restore password controller action call to update_attributes needs to be fixed 
+   end
+
   it "should delete a user" do
    @user = User.find_object("test_user")
    hash = @user.destroy
