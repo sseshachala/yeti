0a27af11fa27b2102d96e9fffe0cd68ade74e48a
obi-a <obioraakubue@yahoo.com>
2012-07-10 01:38:52 -0400

    adding tests

--- spec/models/rmonitor_spec.rb
+++ spec/models/rmonitor_spec.rb
@@ -1,5 +1,29 @@
 require 'spec_helper'
 
 describe Rmonitor do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+
+   it { Rmonitor.should include(ActiveModel::Validations) }
+
+   it "should create a monitor" do
+      hash = {"username" =>"monitor_test_user", "email" => "test@yeti.com","password" => "trusted", "password_confirmation" => "trusted"}
+      current_user = User.new(hash)
+
+      hash = {"every"=>"5m", "test"=>"sample test", "url"=>"http://sample-test.com", "notify_interval"=>"6h"}
+      
+       @rmonitor = Rmonitor.new(hash)
+       @rmonitor.create_monitor(current_user).should == true
+   end
+
+   it "should delete a monitor" do
+  
+     attributes = Rmonitor.find_by_owner("monitor_test_user")     
+     hash =  attributes[0]
+     id = hash["_id"]
+     @rmonitor = Rmonitor.find_object(id)
+
+     hash = @rmonitor.destroy 
+     hash.include?("ok").should == true
+     hash.include?("true").should == true
+   end
 end
