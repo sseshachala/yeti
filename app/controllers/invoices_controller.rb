3b19411ed489780bfd66c3633c2d9ca46f7c5bb7
obi-a <obioraakubue@yahoo.com>
2012-09-21 04:48:23 -0400

    add billing history

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -27,7 +27,11 @@ class InvoicesController < ApplicationController
   end
 
   def billing_history
-
+   if (params[:id] == current_user.attributes["username"]) || current_user.admin?
+      @invoices = Invoice.find_by_owner(params[:id])
+   else
+     redirect_to(show_path("dashboard",  current_user.attributes["username"]))
+   end
   end
 
   # GET /invoices/new
