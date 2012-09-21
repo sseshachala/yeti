6f5c75f4332c6e4b833ae3be7eb51f5c1e7becfb
obi-a <obioraakubue@yahoo.com>
2012-09-21 04:47:50 -0400

    find invoice by owner

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,3 +1,18 @@
 class Invoice 
 
+
+ def self.find(id)
+
+ end
+
+ def self.find_by_owner(username)
+    db = SQLite3::Database.new( '/home/obi/bin/yeti/invoice.db' )
+    db.results_as_hash=true
+    query = "select * from invoice where username = '" + username +"'"
+    puts query.inspect
+    rows = db.execute(query)   
+    #puts rows.inspect
+    return rows
+ end
+
 end
