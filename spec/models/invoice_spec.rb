a61ffa1a5502846f5efe69a4915269c56e31c069
obi-a <obioraakubue@yahoo.com>
2012-09-23 00:02:14 -0400

    completed test coverage for invoice model

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe Invoice do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "should find invoices owned by user" do
+   rows =  Invoice.find_by_owner("yeti")
+   hash = rows[0]
+   hash.include?("id").should == true
+   hash.include?("balance").should == true
+   hash.include?("billing_period").should == true
+   hash.include?("date_due").should == true
+   hash.include?("status").should == true 
+   hash.include?("url_monitoring").should == true 
+
+   rows =  Invoice.find_by_owner("obi-a")
+   rows.should == []
+  end
+
 end
