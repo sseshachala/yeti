dc25bbd2d377b2bc2aa2fdb283ced86e589a80ee
obi-a <obioraakubue@yahoo.com>
2013-01-05 04:28:25 -0500

    run rake secret on new deploys

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,7 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Yeti::Application.config.secret_token = '62f752b0ab0981f1167abb52eef95b234baf2fbe291737c918046080e109457c65566b0294871731014c3a3f092df9669871bdf12df9b0c5e55d299aa3399bbb'
+
+#DO NOT USE the secret key below
+#run rake secret to generate a new one
+#Yeti::Application.config.secret_token = '62f752b0ab0981f1167abb52eef95b234baf2fbe291737c918046080e109457c65566b0294871731014c3a3f092df9669871bdf12df9b0c5e55d299aa3399bbb'
