2eeef0a139a045294af10f2d6bb5c80647ed56af
obi-a <obioraakubue@yahoo.com>
2013-02-24 16:36:44 -0500

    breadcrumbs for static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+
   def home
     if signed_in?
      redirect_to(show_path("dashboard",  current_user.attributes["username"]))
@@ -6,15 +7,23 @@ class PagesController < ApplicationController
   end
 
   def contact
+      contact_url = "/contact"
+      @breadcrumb = {"Contact" => contact_url}
   end
 
   def support
+      support_url = "/support"
+      @breadcrumb = {"Support" => support_url}
   end
 
   def tos
+      tos_url = "/tos"
+      @breadcrumb = {"Terms of Service" => tos_url}
   end
 
   def aboutus
+      about_us_url = "/aboutus"
+      @breadcrumb = {"About us" => about_us_url}
   end
 
 end
