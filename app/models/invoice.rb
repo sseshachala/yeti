57bf7d64a19de22dfa8e66999243628d512bab49
obi-a <obioraakubue@yahoo.com>
2013-04-05 06:00:15 -0400

    disable sqlite, invoice will be rebuilt to use couchDB later

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -6,11 +6,12 @@ class Invoice
  end
 
  def self.find_by_owner(username)
-    db = SQLite3::Database.new( '/home/obi/bin/yeti/invoice.db' )
-    db.results_as_hash=true
-    query = "select * from invoice where username = '" + username +"'"
-    rows = db.execute(query)   
-    return rows
+    #TODO: re-build later using couchDB
+    #db = SQLite3::Database.new(@@invoice_db_path)
+    #db.results_as_hash=true
+    #query = "select * from invoice where username = '" + username +"'"
+    #rows = db.execute(query)   
+    return []
  end
 
 end
