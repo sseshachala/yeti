c952c214a47e7fcfb4c19c5e13a1f2f0a5224b52
obi-a <obioraakubue@yahoo.com>
2013-04-13 04:55:26 -0400

    For the webapp/userspace refer to monitors as websites/website monitors

--- app/models/rmonitor.rb
+++ app/models/rmonitor.rb
@@ -12,8 +12,9 @@ url_regex = /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(
 
 validates :every, :presence => true, :numericality => {:only_integer => true}
 
-validates :test, :presence => true,
+validates :description, :presence => true,
                  :length => {:within => 4..50}
+                 
 
 #validates :contact, :presence => true,
 #                    :format => {:with => email_regex}
@@ -21,12 +22,12 @@ validates :test, :presence => true,
 validates :url, :presence => true,
                 :format => {:with => url_regex}
 
-attr_accessor :attributes,:every, :test,:contact, :url #, :notify_interval
+attr_accessor :attributes,:every, :description,:contact, :url #, :notify_interval
 
  def initialize(attributes = {})
     @attributes = attributes
     @every = @attributes["every"]
-    @test = @attributes["test"]
+    @description = @attributes["description"]
     @contact = @attributes["contact"]
     @url = @attributes["url"]
   end
@@ -47,18 +48,24 @@ def update_attributes(params)
 end
 
 def update_monitor(params)
- str = Yajl::Encoder.encode(make_monitor(params))
+ monitor =  make_monitor(params)
+
+ #Yeti uses decription attribute for Ragios monitor's test attribute so we have to do a conversion
+ test = monitor["description"]
+ monitor = monitor.merge("test" => test )
+ 
+ str = Yajl::Encoder.encode(monitor)
   response = RestClient.put 'http://127.0.0.1:5041/monitors/' + params["id"],str, {:content_type => :json, :accept => :json}
   return true unless response.code != 200 
 end
 
 def create_monitor(owner, params)
-   
+   #Yeti uses decription attribute for Ragios monitor's test attribute so we have to do a translation
    m = make_monitor(params)
    data = [{:monitor => 'metered_url_monitor',
               :tag => owner.username,
              :every => m["every"], 
-              :test => @attributes["test"], 
+              :test => @attributes["description"], 
                 :contact =>  owner.attributes["email"],
                    :url => @attributes["url"],
                          :via => 'ses'}]
@@ -83,6 +90,8 @@ end
 
 def self.find_object(id)
    monitor_hash = find(id)
+   #Yeti uses decription attribute for Ragios monitor's test attribute so we have to do a conversion
+   monitor_hash = monitor_hash.merge("description" => monitor_hash["test"])
    @rmonitor = Rmonitor.new(monitor_hash)
 end
 
