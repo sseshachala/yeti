f8a82ceffda1b06795bde99560aacf42dd4b14db
obi-a <obioraakubue@yahoo.com>
2013-04-29 16:04:45 -0400

    Added google analytics tracking

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
   include ApplicationHelper
   include RmonitorsHelper
+  before_filter :include_ga
+
+  def include_ga
+   @tracking_id = @@tracking_id
+   @ga_sitedomain = @@ga_sitedomain
+  end
 end
